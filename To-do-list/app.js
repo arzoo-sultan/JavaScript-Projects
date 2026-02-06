ae685259add561cd3eb06a6ae090f2c78b57fcef
arzoo-sultan <meers2800@gmail.com>
2026-02-06 19:17:59 +0500

    Refactor task management functions to improve task loading and saving

--- To-do-list/app.js
+++ To-do-list/app.js
@@ -1,6 +1,7 @@
 let taskInput=document.querySelector('#taskInput')
 let addBtn=document.querySelector('#addBtn')
 let taskList=document.querySelector('#taskList')
+loadTasks();
 function addTask(){
     let taskText=taskInput.value.trim();
     if(taskText===""){
@@ -18,6 +19,8 @@ function addTask(){
   taskList.appendChild(li);
   li.appendChild(deleteBtn);
   taskInput.value="";
+  saveTasks();
+
 }
 addBtn.addEventListener("click", addTask);
 
@@ -30,10 +33,48 @@ taskList.addEventListener("click", function(e) {
   if (e.target.tagName === "LI") {
     e.target.classList.toggle("completed");
   }
+  
+
 
   if (e.target.textContent === "❌") {
     e.target.parentElement.remove();
   }
+  saveTasks();
+
 });
+function saveTasks(){
+  const tasks=[];
+  document.querySelectorAll("#taskList li").forEach(li=>{
+    tasks.push({
+      text:li.firstChild.textContent,
+      completed:li.classList.contains("completed")
+    });
+   
+  });
+  localStorage.setItem("tasks",JSON.stringify(tasks));
+}
+ 
+
+function loadTasks(){
+  const tasks=JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks.forEach(task => {
+    const li = document.createElement("li");
+    li.textContent = task.text;
+
+    if (task.completed) {
+      li.classList.add("completed");
+    }
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "❌";
+    deleteBtn.style.border = "none";
+    deleteBtn.style.background = "transparent";
+    deleteBtn.style.cursor = "pointer";
+
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+  });
+
+}
 
 
